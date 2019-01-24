746289a0fb1c93d5c671867b96f7f4a956705248
ab <abazovaa@sch1595.com>
2019-01-24 13:46:31 +0300

    amr

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+// Напишите и вызовите функцию printWindowValues, которая будет выводить в консоль все значения глобального объекта window.
+
 function printWindowValues() {
   console.log(window);
 }
 printWindowValues();
+
+/*Напишите функцию expTen, которая будет возводить в десятую степень
+переданное в фукнцию число с помощью любого цикла (оператор возведения
+в степень использовать нельзя). Выведите в консоль результат функции.*/
+
+function expTen(a){
+var sum = 1;
+for (var i = 0; i < 10; i++){
+  sum*=a;
+}
+return sum;
+}
+console.log (expTen(2));
